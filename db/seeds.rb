ca431b188e7a29a361b74a36274c58ace95e88c6
Astika Luintel <astild2020@gmail.com>
2022-10-30 10:55:14 -0400

    commit tree 57abae43356bd554b7e6b7e8b86b89e541fe2740

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts"Seeding cheeses..."
+Cheese.create(name: "Brie", price: 5, is_best_seller: true)
+Cheese.create(name: "Probolen", price: 10, is_best_seller: true)
+Cheese.create(name: "Ceddar", price: 5, is_best_seller: false)
+
